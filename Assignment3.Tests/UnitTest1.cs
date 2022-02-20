06888f1d568716c5b38031262f0256b959e8916e
Dawn Scott <89325001+dmscott2@users.noreply.github.com>
2022-02-19 19:55:12 -0600

    UpdatedCommit

--- Assignment3.Tests/UnitTest1.cs
+++ Assignment3.Tests/UnitTest1.cs
@@ -1,12 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
+using Assignment3.Tests;
+using Assignment3.Models;
 
-namespace Assignment3.Tests;
-
-public class UnitTest1
+namespace Assignment3
 {
-    [Fact]
-    public void Test1()
+    public class DataAccessLayerTest
     {
+        [Fact]
+        public async Task OnPostAsync_CustomersAreReturned()
+        {
+            using (var db = new DeliveryCartDbContext(Utilities.TestDbContextOptions()))
+            {
+                // Arrange
+                var expectedCustomers = DeliveryCartDbContext.GetSeedingCustomers();
+                await db.AddRangeAsync(expectedCustomers);
+                await db.SaveChangesAsync();
 
+                // Act
+                var result = await db.OnPostAsync();
+
+                // Assert
+                var actualCustomers = Assert.IsAssignableFrom<List<Customer>>(result);
+                Assert.Equal(
+                    expectedCustomers.OrderBy(m => m.Id).Select(m => m.Text), 
+                    actualCustomers.OrderBy(m => m.Id).Select(m => m.Text));
+            }
+        }
     }
 }
